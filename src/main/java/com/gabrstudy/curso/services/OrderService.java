31b389a3160cf791110ad6852ef47df665529a7e
Grey <gabrielnascimentosouza11@gmail.com>
2025-10-15 20:52:06 -0300

    instant, order, ISO 8601 - p2

---
+++ src/main/java/com/gabrstudy/curso/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.gabrstudy.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrstudy.curso.entities.Order;
+import com.gabrstudy.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> FindAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
+
